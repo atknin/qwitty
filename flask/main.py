fa4c2f0fa36e613a9f39095a1d0e65bb8657ac8e
atknin <ivan@atknin.ru>
2019-05-02 18:22:04 +0300

    new

--- flask/main.py
+++ flask/main.py
@@ -1,13 +1,10 @@
 from flask import Flask,render_template
 from werkzeug.contrib.fixers import ProxyFix
-app = Flask(__name__, static_url_path='/home/user/qwitty/flask')
+app = Flask(__name__,static_url_path='/static',template_folder='templates')
 
 @app.route('/')
 def root():
-    try:
-        return render_template('init/index.html')
-    except Exception as e:
-        return e
+    return render_template('init/index.html') 
 
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
