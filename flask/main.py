1e0a9d7ba5cb39fc13203ca036b98ef925d1db09
atknin <ivan@atknin.ru>
2019-05-02 17:42:06 +0300

    index.html

--- flask/main.py
+++ flask/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
-app = Flask(__name__)
- 
+app = Flask(__name__, static_url_path='')
+
 @app.route('/')
-def hello():
-    return "Hello world!"
+def root():
+    return app.send_static_file('init/index.html')
+
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
